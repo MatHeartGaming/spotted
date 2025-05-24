395d1b44529b2278d77e869fe33f25ae019e7701
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-05-24 12:52:57 +0200

    Added flutter_chat_reactions 0.1.1 package

---
+++ lib/presentation/widgets/shared/reactions/reaction_row.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ReactionPostRow extends ConsumerWidget {
+  const ReactionPostRow({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        
+      ],
+    );
+  }
+}
