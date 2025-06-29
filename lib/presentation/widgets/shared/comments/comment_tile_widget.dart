5e19f05cddcef2e159872889c7541d1edbfa9912
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-06-29 17:08:07 +0200

    Removed unuseful import

--- lib/presentation/widgets/shared/comments/comment_tile_widget.dart
+++ lib/presentation/widgets/shared/comments/comment_tile_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:spotted/config/config.dart';
